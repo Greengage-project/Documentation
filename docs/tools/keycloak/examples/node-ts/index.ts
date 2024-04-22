acf90cc9d565696291ed9b355c57014fd6f7b6fc
fvergaracl <fvergaracl@gmail.com>
2024-04-22 11:27:49 +0200

    node + ts example added

--- docs/tools/keycloak/examples/node-ts/index.ts
+++ docs/tools/keycloak/examples/node-ts/index.ts
@@ -1,40 +1,40 @@
-const express = require("express");
-const session = require("express-session");
-const Keycloak = require("keycloak-connect");
-const keycloakSettings = require("./keycloak.json");
-
+import express, { Request, Response } from "express";
+import session from "express-session";
+import Keycloak from "keycloak-connect";
+import keycloakSettings from "./keycloak.json";
+import { RequestKeycloak } from "./custom-types";
 const app = express();
-
 const memoryStore = new session.MemoryStore();
 
+interface CustomKeycloakOptions extends Keycloak.KeycloakOptions {
+  clientId?: string;
+}
+
 const keycloak = new Keycloak({
-  store: memoryStore, // Use memoryStore here
+  store: memoryStore,
   scope: "openid",
   clientId: keycloakSettings["resource"],
   bearerOnly: true,
   serverUrl: keycloakSettings["auth-server-url"],
   realm: keycloakSettings["realm"],
-});
+} as CustomKeycloakOptions);
 
 app.use(
   session({
     secret: keycloakSettings["credentials"]["secret"],
     resave: false,
     saveUninitialized: true,
-    store: memoryStore, // Use memoryStore for session storage
+    store: memoryStore,
   })
 );
 
 app.use(keycloak.middleware());
 
-// / no auth required
-
-app.get("/", (req, res) => {
-  const isAuthenticated = req.kauth.grant !== undefined;
+app.get("/", (req: RequestKeycloak, res: Response) => {
+  const isAuthenticated = req?.kauth?.grant !== undefined;
   if (!isAuthenticated) {
     return res.send("Hello World | <a href='/auth'>Login</a>");
   }
-  // get JWT token
   const token = req?.kauth?.grant?.access_token?.token;
   const decodedToken = req?.kauth?.grant?.access_token?.content;
   res.send(`<p> Logged </p> <a href='/logout'>Logout</a>
@@ -43,14 +43,14 @@ app.get("/", (req, res) => {
   `);
 });
 
-// /auth required
-
-app.get("/auth", keycloak.protect(), (req, res) => {
+app.get("/auth", keycloak.protect(), (_: Request, res: Response) => {
   res.send("Authenticated  | <a href='/logout'>Logout</a>");
 });
 
-app.get("/logout", (req, res) => {
-  req.logout();
+app.get("/logout", (req: RequestKeycloak, res: Response) => {
+  if (req) {
+    req.logout?.();
+  }
   res.redirect("/");
 });
 
