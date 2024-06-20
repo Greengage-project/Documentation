fce9597e8c58c9bfddf02b445c850e911eb64999
fvergaracl <fvergaracl@users.noreply.github.com>
2024-06-20 10:11:34 +0000

    deploy: 7ff3b424774de545ff903bb0f9e340e38be32c2e

--- tools/keycloak/examples/flutter/lib/login_page.dart
+++ tools/keycloak/examples/flutter/lib/login_page.dart
@@ -13,6 +13,8 @@ class _LoginPageState extends State<LoginPage> {
   final FlutterAppAuth _appAuth = FlutterAppAuth();
   String? _accessToken;
   String? _idToken;
+  String? _refreshToken;
+  Map<String, dynamic>? _profile;
 
   Future<void> _login() async {
     // PRINT ENVIRONMENT CONFIG
@@ -36,7 +38,12 @@ class _LoginPageState extends State<LoginPage> {
         setState(() {
           _accessToken = result.accessToken;
           _idToken = result.idToken;
+          _refreshToken = result.refreshToken;
+          _profile = _decodeJWT(_idToken!);
         });
+        if (result.idToken != null) {
+          print('Profile: $_profile');
+        }
       }
     } catch (e) {
       print('Error during login: $e');
@@ -56,12 +63,85 @@ class _LoginPageState extends State<LoginPage> {
           final userInfo = json.decode(response.body);
           print('User info: $userInfo');
         } else {
-          print('Failed to fetch user info');
+          print(
+              'Failed to fetch user info: ${response.statusCode} ${response.body}');
         }
       } catch (e) {
         print('Error fetching user info: $e');
       }
     }
+  }
+
+  Future<void> _logout() async {
+    print('Logging out...');
+    print(_refreshToken);
+    if (_refreshToken != null) {
+      try {
+        final response = await http.post(
+          Uri.parse(
+              'https://auth1.demo.greengage-project.eu/auth/realms/greengage/protocol/openid-connect/logout'),
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          body:
+              'client_id=${Uri.encodeQueryComponent(EnvironmentConfig.clientId)}&refresh_token=${Uri.encodeQueryComponent(_refreshToken!)}',
+        );
+
+        print(' ');
+        print(' ');
+        print(' ');
+        print(_profile);
+        print(' ');
+        print(' ');
+        print(' ');
+
+        if (response.statusCode == 204) {
+          setState(() {
+            _accessToken = null;
+            _idToken = null;
+            _refreshToken = null;
+            _profile = null;
+          });
+          print('Logged out successfully');
+        } else {
+          print('Failed to logout: ${response.statusCode} ${response.body}');
+        }
+      } catch (e) {
+        print('Error during logout: $e');
+      }
+    }
+  }
+
+  Map<String, dynamic> _decodeJWT(String token) {
+    final parts = token.split('.');
+    if (parts.length != 3) {
+      throw Exception('Invalid token');
+    }
+
+    final payload = _decodeBase64(parts[1]);
+    final payloadMap = json.decode(payload);
+    if (payloadMap is! Map<String, dynamic>) {
+      throw Exception('Invalid payload');
+    }
+
+    return payloadMap;
+  }
+
+  String _decodeBase64(String str) {
+    String output = str.replaceAll('-', '+').replaceAll('_', '/');
+
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += '==';
+        break;
+      case 3:
+        output += '=';
+        break;
+      default:
+        throw Exception('Illegal base64url string');
+    }
+
+    return utf8.decode(base64Url.decode(output));
   }
 
   @override
@@ -73,10 +153,33 @@ class _LoginPageState extends State<LoginPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             if (_accessToken != null)
-              Text('Access Token: $_accessToken',
+              ElevatedButton(
+                onPressed: _logout,
+                child: Text('Logout'),
+              ),
+            if (_profile != null) ...[
+              Text('Name: ${_profile!['name']}',
                   style: TextStyle(fontSize: 16)),
-            if (_idToken != null)
-              Text('ID Token: $_idToken', style: TextStyle(fontSize: 16)),
+              Text('Preferred Username: ${_profile!['preferred_username']}',
+                  style: TextStyle(fontSize: 16)),
+              Text('Locale: ${_profile!['locale']}',
+                  style: TextStyle(fontSize: 16)),
+              if (_profile!['picture'] != null)
+                Image.network(_profile!['picture']),
+              Container(
+                margin: EdgeInsets.only(bottom: 16),
+                child: Text('Id: ${_profile!['sid']}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ],
+            Text('Access Token: $_accessToken', style: TextStyle(fontSize: 16)),
+            if (_accessToken != null)
+              if (_idToken != null)
+                Text('ID Token: $_idToken', style: TextStyle(fontSize: 16)),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _login,
@@ -92,4 +195,10 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginPage(),
+  ));
 }
--- tools/keycloak/examples/flutter/test/widget_test.dart
+++ tools/keycloak/examples/flutter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:keycloak_example/main.dart';
+import 'package:keycloaklogin/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
